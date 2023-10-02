aec2ac4c45bd70367e549fc80d329b6a098837f2
unknown <student@L307-08.helium.com>
2023-10-02 18:17:19 +0200

    initial

---
+++ DSP/Debug/c6713dskinit.pp
@@ -0,0 +1,39 @@
+# FIXED
+
+c6713dskinit.obj: ../c6713dskinit.c
+c6713dskinit.obj: ../c6713dskinit.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/dsk6713.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chip.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_stdinc.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_stdinchal.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/tistdtypes.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chiphal.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_emifhal.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chip.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_irq.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_irqhal.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_timer.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_timerhal.h
+c6713dskinit.obj: ../dsk6713_aic23.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_mcbsp.h
+c6713dskinit.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_mcbsphal.h
+
+../c6713dskinit.c: 
+../c6713dskinit.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/dsk6713.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chip.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_stdinc.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_stdinchal.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/tistdtypes.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chiphal.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_emifhal.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chip.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_irq.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_irqhal.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_timer.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_timerhal.h: 
+../dsk6713_aic23.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_mcbsp.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_mcbsphal.h: 
---
+++ DSP/Debug/main.pp
@@ -0,0 +1,35 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/dsk6713.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chip.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_stdinc.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_stdinchal.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/tistdtypes.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chiphal.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_emifhal.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chip.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_irq.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_irqhal.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_timer.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_timerhal.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/dsk6713_led.h
+main.obj: c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/dsk6713_flash.h
+
+../main.c: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/dsk6713.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chip.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_stdinc.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_stdinchal.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/tistdtypes.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chiphal.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_emifhal.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_chip.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_irq.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_irqhal.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_timer.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/csl_timerhal.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/dsk6713_led.h: 
+c:/ti/ccsv6/tools/compiler/c6000_7.4.7/include/dsk6713_flash.h: 
