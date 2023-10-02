813c6473f381eb1a947807d41ca6aec59f20cc5e
unknown <student@L307-08.helium.com>
2023-10-02 18:28:46 +0200

    0

--- DSP/main.c
+++ DSP/main.c
@@ -1,23 +1,27 @@
-#include "dsk6713.h"
-#include "dsk6713_led.h"
-#include "dsk6713_flash.h"
+#include "dsk6713_aic23.h"
+
+#define BUFFERLENGTH 256 
+
+Uint32 fs=DSK6713_AIC23_FREQ_8KHZ; 
+
+int loop = 0; 
+short gain = 10; 
+short sine_table[8]={0,707,1000,707,0,-707,-1000,-707};
+short out_buffer[256]; 
+int i = 0; 
 
 interrupt void c_int11()
 {
+	output_sample(sine_table[loop]*gain); 
+	out_buffer[i] = sine_table[loop]*gain; 
+	i++; 
+	if(i==BUFFERLENGTH) i=0; 
+	if (++loop > 7) loop = 0; 
 	return;
 }
 
-
-void main(void)
+void main()
 {
-	DSK6713_init();
-
-	DSK6713_LED_init();
-	
-	while (1)
-	{
-		DSK6713_LED_toggle(2);
-		DSK6713_waitusec(500000);
-	}
+	comm_intr(); 
+	while(1); 
 }
-
