88316bd51083f07c5b9a2435e29d917f2e6becfb
KczyzCTI <115151929+KczyzCTI@users.noreply.github.com>
2023-10-23 12:40:18 +0200

    Update main.c
    Linear buffer

--- DSP/main.c
+++ DSP/main.c
@@ -16,6 +16,11 @@ Buffer_t jehany_buffer, buhany_jeffer;
 
 short period[LDB_PERIOD];
 
+
+/**
+	Trivial impolementation.
+ 	Both wrap & Buffer_wrap had already been demonstrated
+*/
 short* wrap(int _D, short *w, short *p){
 	if(p-w > _D){
 		return 0;
@@ -24,22 +29,16 @@ short* wrap(int _D, short *w, short *p){
 		return (w+*p);
 	}
 }
+
+/**
+	Our (better) buffer wrap
+ 	The only difference is that our implementation uses struct instead of bare pointers.
+*/
 int Buffer_wrap(unsigned short _D, Buffer_t* buf)
 {
 	buf->rx = _D;
 	if(_D >= LDB_PERIOD*LDB_CAPACITY){
 		_D -= LDB_PERIOD*LDB_CAPACITY;
-	}
-}
-int Buffer_wrapP(unsigned short _D, Buffer_t* buf)
-{
-	short val = LDB_CAPACITY-_D;
-	if (val<0){
-		val =0;
-	}
-	buf->rx = val*LDB_PERIOD;
-	if(buf->rx >= LDB_PERIOD*LDB_CAPACITY){
-		buf->rx = 0;
 	}
 }
 
@@ -56,6 +55,52 @@ interrupt void c_int11()
 	return;
 }
 
+// LINEAR BUFFER DEMO
+typedef struct{
+	unsigned int size;
+	short samples[LDB_PERIOD*LDB_CAPACITY];
+}linear_t;
+void LINEAR_Init(linear_t* buf){
+	buf->size = 0;
+}
+void LINEAR_push(linear_t* buf, short sample){
+	buf->samples[buf->size] = sample;
+	buf->size++;
+	if(buf->size >= LDB_PERDIOD*LDB_CAPACITY){ // overflow, data loss inevitable
+		memset(buf->samples, 0, LDB_PERIOD*LDB_CAPACITY*sizeof(short)); // set all samples to 0
+		buf->size = 0;
+	}
+}
+
+void LINEAR_pop(linear_t* buf, short* sample){
+	if (buf->size == 0){
+		return; // buffer empty
+	}
+	else{
+		buf->size--;
+		*sample = buf->samples[buf->size];
+		buf->samples[buf->size] = 0;
+		
+	}
+}
+
+void LINEAR_shift(unsigned short _D, linear_t* buf){
+	// others will probably use for loop (the slowest way possible)
+	// for CHUNK/BLOCK data transfers memcpy is almost always optimal
+	int diff = buf->size + _D - LDB_PERIOD*LDB_CAPACITY;
+	if(diff < 0){ // _D is too large, data loss inevitable
+		if(-1*diff > buf->size){
+			// way too large
+			return;
+		}
+		else{
+			buf->size += diff;
+		}
+	}
+	memcpy(buf->samples+_D, buf->samples, buf->size*sizeof(short)); // shift samples
+	memset(buf->samples, 0, _D*sizeof(short); // clean after shift
+}
+// END
 void main()
 {
 	Buffer_init(&jehany_buffer);
